16bd60d794416c76bcf7ca0ad3f7e2ea98f6f63e
Bykaii <young@deelol.com>
2019-02-21 12:12:12 +0800

    Fixed model bug.

--- app/model/user.js
+++ app/model/user.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = app => {
-  const { INTEGER, DATE, STRING } = Sequelize
-  await queryInterface.createTable('users',{
+  const { INTEGER, DATE, STRING } = app.Sequelize
+  const User = app.model.define('users',{
     id: { type: INTEGER, primaryKey: true, autoIncrement: true, unique: true },
     email: {type: STRING(100), unique: true },
     password: STRING(100),
