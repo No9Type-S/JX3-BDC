62e480bee66f18929c1f07faf97d2dd06b6eff45
Bykaii <young@deelol.com>
2019-02-21 15:39:16 +0800

    Added the login function in user controller

--- app/controller/users.js
+++ app/controller/users.js
@@ -9,23 +9,73 @@ class UserController extends Controller {
 
   async register(){
     const ctx = this.ctx
-    if(!ctx.request.body.email && !ctx.request.body.password){
-      return ctx.body = {
-        code: 403,
-        message: 'Get none data.'
-      }
+    const { email, password } = ctx.request.body
+    if(!email && !password){
+      let msg = '账号或密码不能为空'
+      ctx.throw(401, msg)
     }
+
     const userData = {
-      email: ctx.request.body.email,
-      password: await ctx.genHash(ctx.request.body.password)
+      email: email,
+      password: await ctx.genHash(password)
     }
+
+    const has = await ctx.model.User.find({
+      where: {
+        email: email
+      }
+    })
+
+    if(has){
+      let msg = '邮箱已经存在'
+      ctx.throw(403, msg)
+    }
+
     const user = await ctx.model.User.create(userData)
-    const token = await ctx.app.jwt.sign(userData, ctx.app.config.jwt.secret, {
+
+    const token = await ctx.app.jwt.sign(userData.email, ctx.app.config.jwt.secret, {
       expiresIn: '1h'
     })
+
+    ctx.status = 201;
     ctx.body = {
-      code: 200,
       message: 'Account created.',
+      token: token
+    }
+  }
+
+  async login() {
+    const ctx = this.ctx
+    const { email, password } = ctx.request.body
+    if(!email && !password){
+      let msg = '请填写账号信息'
+      ctx.throw(400, msg)
+    }
+    const user = await ctx.model.User.find({
+      where: {
+        email: email
+      }
+    })
+
+    let passVerify = false
+
+    if (user.id) {
+      passVerify = await ctx.compare(password, user.password) // verify passowrd
+    }
+
+    if ( !passVerify ) {
+      let msg = '账号或者密码错误'
+      ctx.throw( 400, msg )
+    }
+
+    const token = await ctx.app.jwt.sign(user.email, ctx.app.config.jwt.secret, {
+      expiresIn: '1h'
+    })
+
+    ctx.status = 201;
+    
+    ctx.body = {
+      message: 'Logined',
       token: token
     }
   }
--- app/router.js
+++ app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.post('/register', controller.users.register);
+  router.post('/login', controller.users.login);
 };
